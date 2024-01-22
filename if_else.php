dfabaa8f33a1cabe214c17d9506702debfcf1adf
vijay262000 <121922341+vijay262000@users.noreply.github.com>
2024-01-22 23:11:12 +0530

    Add files via upload

---
+++ if_else.php
@@ -0,0 +1,35 @@
+<?php
+$a=65;
+$b=40;
+
+/*if($a>78){
+
+    echo "a is greater than 78";
+
+}
+else{
+
+    echo "a is less than 78";
+}
+*/
+?>
+
+<?php
+$age=15;
+
+if($age>18){
+
+    echo " you can drink water with chai also alchohol";
+
+}
+elseif($age>16){
+
+     echo"you can drink water with chai only";
+}
+else{
+    echo " you can drink water only";
+}
+
+?>
+
+
